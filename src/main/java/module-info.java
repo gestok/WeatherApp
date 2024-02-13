10a8d5b438cca9514b2ad4a386daa464df358e4e
George Chondromatidis <giorgos.cho@hotmail.com>
2024-02-13 23:16:43 +0200

    Add the rest of the libraries and a LineChartDemo class.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module plh.team1.weatherapp {
     requires javafx.fxml;
     requires okhttp3;
     requires gson;
+    requires java.desktop;
+    requires jfreechart;
+    requires jcommon;
 
     opens plh.team1.weatherapp to javafx.fxml;
     exports plh.team1.weatherapp;
