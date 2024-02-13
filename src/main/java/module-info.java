4ccff5702a2c19501341af6e365c7459261459b2
pallaslaw01 <115006610+pallaslaw01@users.noreply.github.com>
2024-02-13 21:15:30 +0200

    Added dependencies for libraries

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module plh.team1.weatherapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires okhttp3;
+    requires gson;
 
     opens plh.team1.weatherapp to javafx.fxml;
     exports plh.team1.weatherapp;
