976718922f97a1bc9ded24fb67912743e9d6fc0e
George Chondromatidis <giorgos.cho@hotmail.com>
2024-02-22 02:07:50 +0200

    GUI-3 Enrich search tabs functionalities

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module plh.team1.weatherapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
     requires okhttp3;
     requires gson;
     requires java.desktop;
