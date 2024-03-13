70ad442c4a81ba9805e4565e7eac8e442208b5a7
gk2803 <georkyri7@gmail.com>
2024-03-13 17:20:59 +0200

    formatting

--- src/main/java/plh/team1/weatherapp/model/WeatherDataModel.java
+++ src/main/java/plh/team1/weatherapp/model/WeatherDataModel.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package plh.team1.weatherapp.model;
+
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -15,10 +12,9 @@ import jakarta.persistence.Table;
 import plh.team1.weatherapp.serialization.WeatherData;
 import java.util.Date;
 
-
 @Entity
 @Table(name = "WEATHERDATA")
-@NamedQuery(name = "find weatherdata by id", query= "Select s from WeatherDataModel s where s.id = :id")
+@NamedQuery(name = "find weatherdata by id", query = "Select s from WeatherDataModel s where s.id = :id")
 public class WeatherDataModel {
 
     @Column(name = "temperature", nullable = false, length = 150)
@@ -43,14 +39,12 @@ public class WeatherDataModel {
     private String visibility;
     @Column(name = "dateTime")
     private Date dateTime;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long weatherDataId;
     @ManyToOne
     private CityModel cityModel;
-
-    
 
     public WeatherDataModel() {
     }
@@ -59,7 +53,7 @@ public class WeatherDataModel {
         this.temperature = weatherData.getCurrentCondition().getTempC();
         this.windspeed = weatherData.getCurrentCondition().getWindspeed();
         this.uvIndex = weatherData.getCurrentCondition().getUvIndex();
-        this.weatherDesc = weatherData.getCurrentCondition().getWeatherDesc();        
+        this.weatherDesc = weatherData.getCurrentCondition().getWeatherDesc();
         this.date = weatherData.getWeather(0).getDate();
         this.highTemp = weatherData.getWeather(0).getMaxTempC(); //when the model class is created the index for accessing dates is set to 0 
         this.lowTemp = weatherData.getWeather(0).getMinTempC();
@@ -76,8 +70,6 @@ public class WeatherDataModel {
     public void setDateTime(Date dateTime) {
         this.dateTime = dateTime;
     }
-    
-    
 
     public void setDate(String date) {
         this.date = date;
@@ -118,7 +110,6 @@ public class WeatherDataModel {
     public void setWeatherDesc(String weatherDesc) {
         this.weatherDesc = weatherDesc;
     }
-
 
     public long getWeatherDataId() {
         return weatherDataId;
@@ -175,16 +166,10 @@ public class WeatherDataModel {
     public void setVisibility(String visibility) {
         this.visibility = visibility;
     }
-       
-    
-    
 
     @Override
     public String toString() {
         return "WeatherDataModel{" + "temperature=" + temperature + ", windspeed=" + windspeed + ", uvIndex=" + uvIndex + ", weatherDesc=" + weatherDesc + ", date=" + date + ", highTemp=" + highTemp + ", lowTemp=" + lowTemp + ", feelIsLike=" + feelIsLike + ", weatherDataId=" + weatherDataId + ", cityModel=" + cityModel + '}';
     }
-   
-
-
 
 }
